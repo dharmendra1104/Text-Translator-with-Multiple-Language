ccb116827f7c4672dd36420b35e674252cdba832
dharmendra1104 <dharmendra11042@gmail.com>
2024-04-18 11:32:38 +0300

    Updated

--- index.js
+++ index.js
@@ -105,21 +105,20 @@ const toText = document.querySelector(".to-text");
 const exchange = document.querySelector(".exchange");
 const icons = document.querySelectorAll(".icons i");
 
-selectTag.forEach((tag, id) => {
+
+selectTag.forEach(selectTag => {
   for (let countryCode in countries) {
-    //selecting for example english as language ans hindi as language 
-    let selected;
-    if (id == 0 && countryCode == 'en-GB') {
-      selected = "selected"
-    }
-    else if (id == 1 && countryCode == "hi=IN") {
-      selected = "selected"
-    }
-    let option = `<option value="${countryCode}" ${selected}>${countries[countryCode]}</option>`;
-    selectTag.innerHTML += option;
-    tag.insertAdjacentHTML("beforeend", option)  //adding option tag inside select tag
+    let option = document.createElement("option");
+    // let option =`<option value="${countryCode}">${countries[countryCode]}</option>`
+    option.value = countryCode;
+    // console.log(option.value=countryCode)
+    option.textContent = countries[countryCode];
+    selectTag.appendChild(option);
   }
 });
+if (selectTag[1].value === "hi-IN") {
+  selectTag[0].value = "en-GB";
+}
 
 exchange.addEventListener('click', () => {
   //exchange textarea
@@ -127,20 +126,19 @@ exchange.addEventListener('click', () => {
   fromText.value = toText.value
   toText.value = tempText
 
-  //exchange select tag
+  // exchange select tag
   let templang = selectTag[0].value
+  console.log(templang)
   selectTag[0].value = selectTag[1].value
   selectTag[1].value = templang
-
+  
   function rotateElement() {
     const currentRotation = parseInt(exchange.style.transform.replace('rotate(', '')) || 0;
     const newRotation = currentRotation + 270;
     exchange.style.transform = `rotate(${newRotation}deg)`;
   }
   rotateElement()
-
 })
-
 
 icons.forEach(icon => {
   icon.addEventListener('click', (event) => {
@@ -164,6 +162,7 @@ icons.forEach(icon => {
     }
   });
 });
+
 
 translateBtn.addEventListener('click', () => {
   let text = fromText.value;
